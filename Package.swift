9f05273b65e303645444131d16f8df8a649a094c
Dmitry Nuzhin <iruirc@gmail.com>
2024-04-09 19:08:35 +0300

    lib 'CameraPermission' also .dynamic

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
     products: [
         .library(
             name: "CameraPermission",
+            type: .dynamic,
             targets: ["CameraPermission"]
         ),
         .library(
